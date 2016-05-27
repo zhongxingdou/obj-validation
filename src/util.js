fb56a00a8e2151cca0bb65d95d5303bd37b41c4c
zhongxingdou <zhongxingdou@gmail.com>
2016-05-28 01:06:32 +0800

    重构 checkers 的消息生成，重命名 remote 为 async  并重构它，不依赖 jQuery

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-function format(temp) {
+function format (temp) {
     var data = Array.prototype.slice.call(arguments, 1)
     for (var i = 0, l = data.length; i < l; i++) {
         temp = temp.replace(new RegExp('\\{' + i + '\\}', 'g'), data[i])
@@ -6,6 +6,28 @@ function format(temp) {
     return temp
 }
 
+function utf8Length (str) {
+    var s = str.length
+    for (var i = str.length - 1; i >= 0; i--) {
+        var code = str.charCodeAt(i)
+        if (code > 0x7f && code <= 0x7ff) {
+            s++
+        } else if (code > 0x7ff && code <= 0xffff) {
+            s += 2
+        }
+        if (code >= 0xDC00 && code <= 0xDFFF) {
+            i--
+        }
+    }
+    return s
+}
+
+function hasValue (value) {
+    return value !== undefined && value !== null && value !== ''
+}
+
 export default {
-    format: format
+    format: format,
+    utf8Length: utf8Length,
+    hasValue: hasValue
 }
