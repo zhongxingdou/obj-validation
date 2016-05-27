dc44714a8de587b260a6d3fcacc65446203835f9
zhongxingdou <zhongxingdou@gmail.com>
2016-05-28 01:03:56 +0800

    实现一个简单的 i18n

---
+++ src/i18n.js
@@ -0,0 +1,22 @@
+var localeDict = {}
+var DEFAULT_LOCALE = 'en'
+var currLocale = DEFAULT_LOCALE
+var currDict = {}
+
+var i18n = {
+  setCurrLocale: function (locale) {
+    currLocale = locale
+    currDict = localeDict[currLocale]
+  },
+  getLocaleString: function (key) {
+    return currDict[key]
+  },
+  addLocale: function (locale, dict) {
+    localeDict[locale] = Object.assign(
+      {}, localeDict[locale], dict
+    )
+    this.setCurrLocale(locale)
+  }
+}
+
+export default i18n
