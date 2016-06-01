a01653d4aa96bee310904e27059bc6d916a51824
zhongxingdou <zhongxingdou@gmail.com>
2016-06-01 20:57:27 +0800

    移除全局属性 checkers 并格式化

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import validator   from './validator'
-import validateForm  from './validateForm'
-import checkers    from './checkers'
-import i18n        from './i18n'
-import vueMixin    from './vueMixin'
+import validator    from './validator'
+import validateForm from './validateForm'
+import checkers     from './checkers'
+import i18n         from './i18n'
+import vueMixin     from './vueMixin'
 
 var ObjValidation = validator
-ObjValidation.i18n = i18n
-ObjValidation.checkers = checkers
 
+// add static member
+ObjValidation.i18n = i18n
+ObjValidation.validateForm = validateForm
+ObjValidation.vueMixin = vueMixin
+
+ObjValidation.addChecker(checkers)
+
+// i18n
 import zhLocales from './locales/zh'
 i18n.addLocale('zh', zhLocales)
-
 i18n.setCurrLocale('en')
-
-ObjValidation.validateForm = validateForm
-
-ObjValidation.vueMixin = vueMixin
 
 export default ObjValidation
